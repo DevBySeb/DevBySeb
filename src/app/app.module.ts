fcd3b39ca71cf938e53098f55a80053f6f6beb2d
Sebastian Persson <sebbe-p@hotmail.com>
2023-03-06 19:44:59 +0100

    Add initial files for lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { HomeComponent } from './core/components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/components/home/home.component.html
@@ -0,0 +1,3 @@
+<div>
+  <h1>Home page</h1>
+</div>
