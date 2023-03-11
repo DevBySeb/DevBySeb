30251abefb89e56057e122b49839b653969de80b
Sebastian Persson <sebbe-p@hotmail.com>
2023-03-11 20:40:42 +0100

    adding files for reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular';
+  myForm: FormGroup;
+  myFormArray: FormArray;
+  control = new FormControl('');
+
+  get formArrayControls(): FormGroup[] {
+    return this.myFormArray.controls as FormGroup[];
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.createFormGroup();
+    this.myFormArray = this.fb.array([this.createFormGroup()]);
+  }
+
+  createFormGroup(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  submit(): void {
+    console.log(this.myFormArray.value);
+    if (this.myFormArray.invalid) return;
+  }
+
+  addFormItem(): void {
+    this.myFormArray.push(this.createFormGroup());
+  }
+
+  removeFormItem(index: number): void {
+    this.myFormArray.removeAt(index);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +7,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
