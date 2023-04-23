2b7620857a0183f8d231b98eff7f83cbd079d218
Sebastian Persson <sebbe-p@hotmail.com>
2023-04-23 07:53:30 +0200

    Event bindings in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular';
+
+  callMyFunction() {
+    alert('Hello World');
+  }
+
+  onKeyUp($event: Event) {
+    alert($event);
+  }
+
+  menuWasOpened(isOpened: boolean): void {
+    alert(isOpened);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @Output() menuOpenedState = new EventEmitter<boolean>();
   isMenuOpen = false;
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+
+    this.menuOpenedState.emit(this.isMenuOpen);
   }
+
+  menuWasOpened(): void {}
 }
